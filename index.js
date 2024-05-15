78239d770a52bab64ac9a64b8f462194949d80f2
Nebulous <88895551+NebulousCalm@users.noreply.github.com>
2024-05-15 11:32:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
+const app = express();
+
+app.use('/static/', express.static("./static"));
+app.use(express.urlencoded({ extended: true }));
+app.engine('html', ejs.renderFile);
+
+
+app.get('/', (req, res) =>{
+  res.render('index.html');
+});
